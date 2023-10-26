1c63017fd1ca613000864e860d944bfe10fcb00a
Kevis <kevis@Zipcoders-MacBook-Pro-82.local>
2023-10-25 20:44:10 -0400

    Working

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
+        Person person1 = new Person()
         Logger logger=Logger.getLogger(Main.class.getName());
         logger.info("This is a module-using Hello World!");    }
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,17 @@
+package object1;
+
+public class Person {// Define a class
+    class Person {
+        String name;
+        int age = 0;
+
+        // Constructor
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    // Declare and initialize an object
+    Person person1 = new Person("Kevis", 38);
+}
