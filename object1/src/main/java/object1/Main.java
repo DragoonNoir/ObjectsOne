0ecdd9b686d9106b1f7a708a1f23cfe1d60956a6
Kevis <kevis@Zipcoders-MacBook-Pro-82.local>
2023-11-03 19:14:12 -0400

    Finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -4,8 +4,9 @@ import java.util.logging.Logger;
 
 public class Main {
 
+
     public static void main(String... args) {
-    Person person1 = new Person("Kevis", 38, 74);
+    Person person1 = new Person("Kevis", 38, 74); //unit in cm
     Logger logger=Logger.getLogger(Main.class.getName());
     logger.info("This is a module-using Hello World!");
     logger.info(person1.getHeight() + " ");    }
